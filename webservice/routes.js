0680b40b28c401e54fe93af1bcaa426f88753204
mvrush <mvrush@gmail.com>
2024-01-24 21:33:36 -0700

    HTML Template

--- webservice/routes.js
+++ webservice/routes.js
@@ -1,1 +1,22 @@
-exports.someText = 'Some hard coded text';
+// This is our routes.js file
+
+
+const { request } = require('http');
+
+
+// Following is my 'requestHandler()' function
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method; // We need this to look for the method used (GET or POST).
+
+    // For now, I'm just making sure that the browser can receive data from the Node.js server.
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!<h1></body>');
+    res.write('</html>');
+    res.end();
+};
+
+exports.handler = requestHandler; // This exports our 'requestHandler()' function.
+exports.someText = 'Some hard coded text'; // sends this text to check if our server is running.
